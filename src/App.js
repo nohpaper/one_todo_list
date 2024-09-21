d56c845105c72adddc6579b2551c7f89883434c6
nohpaper <nohpaper99@naver.com>
2024-09-21 17:36:06 +0900

    build: project init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import OneTodo from "./components/todo";
+
+function App() {
+  return (
+    <div className="App w-[100%] h-[100vh] flex items-center justify-center bg-gray-200">
+      <OneTodo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,7 @@
+export default function OneTodo(){
+    return (
+        <div className="wrap w-[300px] h-[60vh] border-8 border-gray-100 rounded-[30px] bg-white shadow-2xl">
+            <div className=""></div>
+        </div>
+    )
+}
